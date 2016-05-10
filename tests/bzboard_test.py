996a1a3522688d716f56ba61257595ae691b5e93
Kevin Donkers <1002778d@student.gla.ac.uk>
2016-05-10 11:04:51 +0100

    example added of how to run pattern of motors

--- tests/bzboard_test.py
+++ tests/bzboard_test.py
@@ -6,6 +6,7 @@ import bzboard
 import time
 
 SETUP_CONFIG_FILE = './bz_setup_config_test.json'
+PATTERN_FILE = './pattern_test.json'
 
 bz = bzboard.BZBoard.from_configfile(SETUP_CONFIG_FILE)
 
@@ -13,3 +14,7 @@ for m in bz.motors:
     bz.activate(m)
     time.sleep(0.5)
     bz.deactivate(m)
+
+# load and run a pattern from .json file
+pattern = bz.pattern_from_file(PATTERN_FILE)
+bz.activate_pattern(pattern)
