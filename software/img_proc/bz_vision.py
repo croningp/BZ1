9a6df51356f263a2ef663cb829467203ca104fc2
Juan Manuel Parrilla Gutierrez <juanma@chem.gla.ac.uk>
2017-03-22 19:04:32 +0000

    Starting with the computer vision part. So far just detecting callbacks from the mouse in order to define the grid where the cells are

---
+++ software/img_proc/bz_vision.py
@@ -0,0 +1,68 @@
+###############################################################################
+#
+# Mouse callback based on: 
+# http://docs.opencv.org/3.1.0/db/d5b/tutorial_py_mouse_handling.html
+# I dont like using globals but they were using them in this example...
+#
+###############################################################################
+
+import numpy as np
+import cv2
+
+
+drawing = False # true if mouse is pressed
+ix,iy = -1,-1
+video = cv2.VideoCapture('test.avi')
+points = [0, 0, 0, 0]
+
+def draw_circle(event, x, y, flags, param):
+    '''mouse callback function'''
+    
+    global ix, iy, drawing, points
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        points = [x, y, x, y]
+        ix,iy = x,y
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+            points = [ix, iy, x, y]
+
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        points = [ix, iy, x, y]
+
+def get_platform_corners(frame):
+    '''Given a frame, it will let the user click on the platform corners
+    in order to obtain its coordinates: top left corner, bottom right corner'''
+
+    cv2.namedWindow('blue channel')
+    cv2.setMouseCallback('blue channel', draw_circle)
+    unmodified = frame.copy()
+
+    while(True):
+        frame = unmodified.copy()
+        x1, y1, x2, y2 = points
+        cv2.rectangle(frame, (x1,y1), (x2,y2), (0,0,255), 3)
+        cv2.imshow('blue channel', frame)
+        cv2.waitKey(10)
+
+while(True):
+
+    ret, frame = video.read()
+    
+    if ret is False:
+        break
+        
+    get_platform_corners(frame)
+
+    # we focus on the blue channel because the reaction changes from red to blue
+    blue_channel = frame[:,:,0]
+
+    cv2.imshow('blue channel', frame)
+    cv2.waitKey(10) # 33 means roughly 30FPS 
+
+video.release()
+cv2.destroyAllWindows()
+
