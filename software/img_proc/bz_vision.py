4b51fbebdd9759bfbaccfcb827ca39dad3699851
Juan Manuel Parrilla Gutierrez <juanma@chem.gla.ac.uk>
2017-03-23 12:18:09 +0000

    Drawing a grid instead of a rectangle

--- software/img_proc/bz_vision.py
+++ software/img_proc/bz_vision.py
@@ -15,7 +15,7 @@ ix,iy = -1,-1
 video = cv2.VideoCapture('test.avi')
 points = [0, 0, 0, 0]
 
-def draw_circle(event, x, y, flags, param):
+def mouse_grid(event, x, y, flags, param):
     '''mouse callback function'''
     
     global ix, iy, drawing, points
@@ -33,18 +33,33 @@ def draw_circle(event, x, y, flags, param):
         drawing = False
         points = [ix, iy, x, y]
 
+
+def draw_grid(frame, points):
+    '''draws a 5x5 grid, just the lines. Used for visualization'''
+
+    x1, y1, x2, y2 = points
+    cv2.rectangle(frame, (x1,y1), (x2,y2), (0,0,255), 3)
+    height = y2 - y1
+    width = x2 - x1
+    stepw = int(width / 5)
+    steph = int(height / 5)
+
+    for i in range(1, 5):
+        cv2.line(frame, (x1+stepw*i,y1),  (x1+stepw*i,y2), (0,0,255), 3)
+        cv2.line(frame, (x1,y1+steph*i),  (x2,y1+steph*i), (0,0,255), 3)
+
+
 def get_platform_corners(frame):
     '''Given a frame, it will let the user click on the platform corners
     in order to obtain its coordinates: top left corner, bottom right corner'''
 
     cv2.namedWindow('blue channel')
-    cv2.setMouseCallback('blue channel', draw_circle)
+    cv2.setMouseCallback('blue channel', mouse_grid)
     unmodified = frame.copy()
 
     while(True):
         frame = unmodified.copy()
-        x1, y1, x2, y2 = points
-        cv2.rectangle(frame, (x1,y1), (x2,y2), (0,0,255), 3)
+        draw_grid(frame, points)
         cv2.imshow('blue channel', frame)
         cv2.waitKey(10)
 
