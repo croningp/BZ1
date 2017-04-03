6226f811c3498fabc10e19a90fb66d8c64cdc08a
Juan Manuel Parrilla Gutierrez <juanma@chem.gla.ac.uk>
2017-04-03 18:20:19 +0100

    Test and train svm are now more modular, and it should be easier to implement another ways of representing the data. So far at this point there is only one example using the blue channel

--- software/img_proc/test_svm.py
+++ software/img_proc/test_svm.py
@@ -14,45 +14,78 @@ import sys
 from generate_dataset import GridClickData
 
 
+class TestSVM():
+    '''This class is just the framework to test the SVM
+    You must instantiate one of the other classes instead'''
 
-def paint_cells(frame, bz_coordinates, svm):
-    ''' Given a frame and the bz coordinates, it extracts the cells 
-    and uses the SVM to predict if it will be a blue or red cell'''
 
-    # top right and bottom right corner of the bz platform
-    x1, y1, x2, y2 = bz_coordinates
-    height = y2 - y1
-    width = x2 - x1
-    stepw = int(width / 5)
-    steph = int(height / 5)
+    def __init__(self, svm_file):
 
-    for i in range(5):
-        for j in range(5):
-            # top left corner of the current cell (i,j)
-            pointA = (x1+9 + stepw * i, y1+9 + steph * j)
-            # bottom right corner of the current cell (i,j)
-            pointB = (x1-9 + stepw * (i+1), y1-9 + steph * (j+1))
-            # Define the Region of Interest as the current cell (i, j)
-            roi = frame[pointA[1]:pointB[1], pointA[0]:pointB[0]]
-            # We will only focus on the blue channel
-            roi_blue = roi[:,:,0]
-            # Resize it to 25,25 as used while training
-            roi_resized = cv2.resize(roi_blue, (25,25))
-            # And flatten it into a 1D array as require by the SVM
-            roi1D = roi_resized.reshape(-1)
-            result = svm.predict(np.array([roi1D], dtype=np.float32))
+        self.svm = cv2.ml.SVM_load(svm_file)
 
-            if int(result[1][0][0]) == 0:
-                # if predicted as 0, paint it blue, otherwise red
-                cv2.rectangle(frame, pointA, pointB, (255,0,0), -1) 
-            else:
-                cv2.rectangle(frame, pointA, pointB, (0,0,255), -1) 
 
+    def paint_cells(self, frame, bz_coordinates):
+        ''' Given a frame and the bz coordinates, it extracts the cells 
+        and uses the SVM to predict if it will be a blue or red cell'''
+
+        # top right and bottom right corner of the bz platform
+        x1, y1, x2, y2 = bz_coordinates
+        height = y2 - y1
+        width = x2 - x1
+        stepw = int(width / 5)
+        steph = int(height / 5)
+
+        for i in range(5):
+            for j in range(5):
+                # top left corner of the current cell (i,j)
+                pointA = (x1+9 + stepw * i, y1+9 + steph * j)
+                # bottom right corner of the current cell (i,j)
+                pointB = (x1-9 + stepw * (i+1), y1-9 + steph * (j+1))
+                # Define the Region of Interest as the current cell (i, j)
+                roi = frame[pointA[1]:pointB[1], pointA[0]:pointB[0]]
+
+                if self.roi_to_decision(roi) == 0:
+                    # if predicted as 0, paint it blue, otherwise red
+                    cv2.rectangle(frame, pointA, pointB, (255,0,0), -1) 
+                else:
+                    cv2.rectangle(frame, pointA, pointB, (0,0,255), -1) 
+
+
+    def roi_to_decision(self, roi):
+        ''' This is the function that every specific class must provide.
+        Given a region of interest (or roi) it will use the SVM to decide
+        if it is red or blue'''
+
+        raise NameError('Use a specific class instead')
+
+
+
+class BlueChannel(TestSVM):
+    '''This class must be used when BlueChannel was used to train the SVM.
+    In this case, the SVM is trained and tested only using the blue channel
+    of the image, and reducing it to 25 by 25 pixels'''
+
+    def ___init___(self, svm_file):
+        super().__init__(svm_file)
+
+
+    def roi_to_decision(self, roi):
+        
+        # We will only focus on the blue channel
+        roi_blue = roi[:,:,0]
+        # Resize it to 25,25 as used while training
+        roi_resized = cv2.resize(roi_blue, (25,25))
+        # And flatten it into a 1D array as require by the SVM
+        roi1D = roi_resized.reshape(-1)
+        # And use the svm to decide if it is red or blue
+        decision = self.svm.predict(np.array([roi1D], dtype=np.float32))
+
+        return int(decision[1][0][0]) 
 
 
 if __name__ == "__main__":
 
-    svm = cv2.ml.SVM_load("svm_data.dat")
+    bc_svm = BlueChannel("svm_bluechannel.dat")
 
     video = cv2.VideoCapture(sys.argv[1])
     click_grid = GridClickData()    
@@ -76,7 +109,7 @@ if __name__ == "__main__":
         # "click_grid" is now populated with the x,y corners of the platform
         click_grid.draw_grid(frame)
         # we use the svm to decide if the cells are painted red or blue
-        paint_cells(frame, click_grid.points, svm)
+        bc_svm.paint_cells(frame, click_grid.points)
 
         cv2.imshow('Video', frame)
         out.write(frame)
--- software/img_proc/train_svm.py
+++ software/img_proc/train_svm.py
@@ -9,43 +9,86 @@ import glob
 import cv2
 import numpy as np
 
-# if we used generate_dataset.py  we should have a "reds" and "blues" folder 
-# with the images we clicked for training. We load then only the blue channel
-blues = [cv2.imread(file)[:,:,0] for file in glob.glob("blues/*.png")]  
-reds = [cv2.imread(file)[:,:,0] for file in glob.glob("reds/*.png")] 
-# we also convert them to 25 by 25, so all of them have the same size
-blues = np.array([cv2.resize(img, (25,25)) for img in blues], dtype=np.float32)
-reds = np.array([cv2.resize(img, (25,25)) for img in reds], dtype=np.float32)
-# and we put everything together into a dataset
-dataset = np.concatenate([blues,reds]) 
-# and we make every image a flat 1D array
-dataset = np.array([img.reshape(-1) for img in dataset])
-# responses will be their "class", 0 for blues, and 1 for reds
-responses = np.array([[0]] * len(blues) + [[1]] * len(reds), dtype=np.int32) 
 
-# no we will divide dataset between train and set
-# first we shuffle both dataset and responses
-# we need to create because we want both shuffles to be identical
-seed = np.random.randint(np.iinfo(np.int32).max)
-np.random.seed(seed)
-np.random.shuffle(dataset)
-np.random.seed(seed)
-np.random.shuffle(responses)
-# If train = test, it is because we are testing now with actual videos, so we
-# used all the data to train
-trainData = dataset
-testData = dataset
 
-# see the link in the header to see how the svm is init. we also use the same
-# parameters for C and gamma
-svm = cv2.ml.SVM_create()
-svm.setKernel(cv2.ml.SVM_LINEAR)
-svm.setType(cv2.ml.SVM_C_SVC)
-svm.setC(2.67)
-svm.setGamma(5.383)
-svm.train(trainData, cv2.ml.ROW_SAMPLE, responses)
-svm.save('svm_data.dat')
-result = svm.predict(testData)
-mask = result[1]==responses
-correct = np.count_nonzero(mask)
-print( correct*100.0/len(result[1]) )
+class TrainSVM:
+    '''This class is the abstract one to train SVMs. Use one of the specific
+    ones instead'''
+
+
+    def __init__(self, dataset, responses):
+
+        self.dataset = dataset
+        self.responses = responses
+
+
+    def train(self, file_to_save):
+        '''Trains the SVM with the dataset provided, and stores it in
+        file_to_save'''
+
+        # first we shuffle both dataset and responses
+        # we create a seed because we want both shuffles to be identical
+        seed = np.random.randint(np.iinfo(np.int32).max)
+        np.random.seed(seed)
+        np.random.shuffle(self.dataset)
+        np.random.seed(seed)
+        np.random.shuffle(self.responses)
+
+        # now we separate dataset between train and test
+        # If train = test, it is because we are testing now with actual videos
+        # so we used all the data to train
+        trainData = self.dataset 
+        testData = self.dataset
+
+        # see the link in the header to see how the svm is init. 
+        # we also use the same parameters as they do for C and gamma
+        svm = cv2.ml.SVM_create()
+        svm.setKernel(cv2.ml.SVM_LINEAR)
+        svm.setType(cv2.ml.SVM_C_SVC)
+        svm.setC(2.67)
+        svm.setGamma(5.383)
+        svm.train(trainData, cv2.ml.ROW_SAMPLE, responses)
+        svm.save(file_to_save)
+        result = svm.predict(testData)
+        mask = result[1]==responses
+        correct = np.count_nonzero(mask)
+        print( correct*100.0/len(result[1]) )
+    
+
+
+class BlueChannel(TrainSVM):
+    ''' This class trains the SVM just using the blue channel of the cells,
+    after converting them into a 25 by 25 pixel image.
+    This is one of the simplest representations, and the svm implementation
+    follows the opencv documentation. See the opening file commet.'''
+
+
+    def __init__(self, dataset, responses):
+
+        super().__init__(dataset, responses)
+
+        # only take blue channel from dataset
+        bc =  [img[:,:,0] for img in self.dataset]  
+        # transform it into 25 by 25
+        b25 =  np.array([cv2.resize(i, (25,25)) for i in bc], dtype=np.float32)
+        # flatten it into 1D
+        self.dataset = np.array([img.reshape(-1) for img in b25])
+
+
+
+if __name__ == "__main__":
+
+    
+    # if we used generate_dataset.py  we should have a "reds" and "blues" folder 
+    # with the images we clicked for training
+    blues = [cv2.imread(file) for file in glob.glob("blues/*.png")]  
+    reds = [cv2.imread(file) for file in glob.glob("reds/*.png")] 
+
+    # and we put everything together into a dataset
+    dataset = blues + reds 
+
+    # responses will be their "class", 0 for blues, and 1 for reds
+    responses = np.array([[0]] * len(blues) + [[1]] * len(reds), dtype=np.int32) 
+
+    bluechannel_svm = BlueChannel(dataset, responses)
+    bluechannel_svm.train("svm_bluechannel.dat")
