dc200fe7b668b94d84299732bf06079ab9b2c82c
Juan Manuel Parrilla <juanma@chem.gla.ac.uk>
2017-04-13 17:05:20 +0100

    Adding new patterns

--- software/bzboard.py
+++ software/bzboard.py
@@ -23,12 +23,23 @@ class BZBoard:
         self.ser.flush(); self.ser.flushInput(); self.ser.flushOutput();
 
         self.motors = { # [adafruit_shield, pin, min_speed]
-            "A1":[1, 8, 110],"A2":[1, 9, 165],"A3":[1,10, 125],"A4":[1,11, 110],"A5":[1,12, 135],
-            "B1":[1,14, 130],"B2":[1,15, 180],"B3":[1,13, 120],"B4":[1, 0, 120],"B5":[1, 1, 170],
-            "C1":[1, 2, 135],"C2":[1, 3, 125],"C3":[1, 4, 170],"C4":[1, 5, 195],"C5":[1, 6, 140],
-            "D1":[1, 7, 130],"D2":[0,4, 130],"D3":[0, 8, 120],"D4":[0, 9, 100],"D5":[0,10, 120],
-            "E1":[0,11, 105],"E2":[0,12, 100],"E3":[0,13, 130],"E4":[0,14, 135],"E5":[0,15, 130]
+            "A1":[1, 8, 500],"A2":[1, 9, 500],"A3":[1,10, 500],"A4":[1,11, 500],"A5":[1,12, 500],
+            "B1":[1,14, 500],"B2":[1,15, 500],"B3":[1,13, 380],"B4":[1, 0, 500],"B5":[1, 1, 500],
+            "C1":[1, 2, 500],"C2":[1, 3, 500],"C3":[1, 4, 500],"C4":[1, 5, 500],"C5":[1, 6, 500],
+            "D1":[1, 7, 500],"D2":[0,4, 500],"D3":[0, 8, 500],"D4":[0, 9, 500],"D5":[0,10, 500],
+            "E1":[0,11, 380],"E2":[0,12, 500],"E3":[0,13, 500],"E4":[0,14, 500],"E5":[0,15, 380]
             }
+
+        # this matrix stores if the motors are enabled (1) or disabled (0)
+        # before writting to serial we check this to not sent writes not needed
+        self.matrix = { 
+            "A1":0,"A2":0,"A3":0,"A4":0,"A5":0,
+            "B1":0,"B2":0,"B3":0,"B4":0,"B5":0,
+            "C1":0,"C2":0,"C3":0,"C4":0,"C5":0,
+            "D1":0,"D2":0,"D3":0,"D4":0,"D5":0,
+            "E1":0,"E2":0,"E3":0,"E4":0,"E5":0
+            }
+
     
 
     def __del__(self):
@@ -59,8 +70,26 @@ class BZBoard:
         else:
             shield, pin, _  = self.motors[motor_code] 
 
-        command = "A%d P%d S%d\n" % ( shield, pin, speed )
-        self.ser.write(command.encode())
+        # because sometimes we can send here a speed 0 to disable it
+        # the following if will be longer than expected
+        if self.matrix[motor_code] < 1 and speed > 0: # we need to activate it
+            #The following line is a dirty trick. Some of the motors need to be
+            # kickstarted at a higher speed
+            command = "A%d P%d S2000\n" % (shield, pin)
+            # and then we send the desired speed
+            command += "A%d P%d S%d\n" % ( shield, pin, speed )
+            self.matrix[motor_code] = 1 # mark it as enabled
+            self.ser.write(command.encode())
+
+        elif self.matrix[motor_code] < 1 and speed == 0: # we keep it disabled
+            pass
+
+        if self.matrix[motor_code] > 0 and speed > 0: # just update speed
+            command = "A%d P%d S%d\n" % ( shield, pin, speed )
+            self.ser.write(command.encode())
+
+        if self.matrix[motor_code] > 0 and speed == 0: # we disable it
+            board.disable_motor(motor_code)
 
 
     def activate_all(self, speed=None):
@@ -84,8 +113,10 @@ class BZBoard:
 
         shield, pin, _ = self.motors[motor_code]
         command = "A%d P%d S0\n" % ( shield, pin )
-        self.ser.write(command.encode())
 
+        if self.matrix[motor_code] > 0: # so it was enabled
+            self.ser.write(command.encode())
+            self.matrix[motor_code] = 0 # mark it disabled
     
     def disable_all(self):
 
