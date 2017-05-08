500acae9d37d06b5550efd5bcd6383d78bce45b4
Juan Manuel Parrilla <juanma@chem.gla.ac.uk>
2017-05-08 18:49:45 +0100

    Record cam fixing bugs, adding read from argv for the timing, and calculating the right waitKey

--- software/img_proc/record_cam.py
+++ software/img_proc/record_cam.py
@@ -4,7 +4,7 @@
 
 import cv2
 import threading
-import time
+import time, random, string, sys
 
 
 def random_filename(size):
@@ -18,11 +18,16 @@ def kill_video(event, time_to_wait):
     '''Waits for time_wait time.
     Then sets and event so that the timeout is marked and the video recording
     stops'''
-    time.sleep(time_to_wait)
+    time.sleep(time_to_wait+1) # cam always takes around 1s to warm up
     event.set()
 
 
 if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        user_time = float(sys.argv[1])
+    else:
+        user_time = 0
 
     vc = cv2.VideoCapture(0)
     vc.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
@@ -34,18 +39,22 @@ if __name__ == "__main__":
     out = cv2.VideoWriter(random_name,fourcc, 30.0, (800,600))
 
     event = threading.Event()
-    timer = threading.Thread(target=kill_video, args=(event,5))
-    timer.start()
+    
+    if user_time > 0:
+        timer = threading.Thread(target=kill_video, args=(event,user_time))
+        timer.start()
 
     while(1):
+        start_time = time.clock() * 1000 # seconds to ms
         ret,frame = vc.read()
         out.write(frame)
         cv2.imshow("video",frame)
-        key =  cv2.waitKey(30)
+
+        end_time = time.clock() * 1000
+        key = cv2.waitKey(34 - int(end_time - start_time))
 
         if key == ord('q') or event.is_set():
             break
-
 
     vc.release()
     out.release()
