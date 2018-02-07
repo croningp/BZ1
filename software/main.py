09f96917b24338ec15c9166ded750f8592c52119
Juan Manuel Parrilla <juanma@chem.gla.ac.uk>
2018-02-07 17:34:59 +0000

    Working on the new main

--- software/main.py
+++ software/main.py
@@ -9,28 +9,6 @@ from bzboard.bzboard import BZBoard
 from generate_dataset import bz_average_color
 
 
-def activate_motors(motors, board, matrix):
-    '''activates the motors from the list given'''
-
-    code = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E'}
-
-    for m in motors:
-        i, j = m[0], m[1]
-        motor = code[i] + str(5-j)
-        if matrix[motor] == 0:
-            matrix[motor] = 1
-            board.activate_motor(motor)
-
-
-# this matrix stores if the motors are enabled (1) or disabled (0)
-# before writting to serial we check this to not sent writes not needed
-matrix = { 
-    "A1":0,"A2":0,"A3":0,"A4":0,"A5":0,
-    "B1":0,"B2":0,"B3":0,"B4":0,"B5":0,
-    "C1":0,"C2":0,"C3":0,"C4":0,"C5":0,
-    "D1":0,"D2":0,"D3":0,"D4":0,"D5":0,
-    "E1":0,"E2":0,"E3":0,"E4":0,"E5":0
-    }
 
 board = BZBoard('/dev/ttyACM0')
 svm = test_svm.HSVHistogramBkgMem('img_proc/hsvhistmem.dat')
