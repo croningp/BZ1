6d951bf1392b5bf3fce3aef7c567a994e8f68370
Juan Manuel Parrilla Gutierrez <juanma@chem.gla.ac.uk>
2018-02-07 14:24:27 +0000

    Adding a lock around the serial read. It seems to work now, not tested in the platform

--- software/pumpsctl/PumpsCtl.py
+++ software/pumpsctl/PumpsCtl.py
@@ -10,34 +10,44 @@ class PumpsCtl:
         sleep(2) # pyserial recommends 2 seconds wait after connection
         self.pumps_ser.flush(); self.pumps_ser.flushInput(); self.pumps_ser.flushOutput();
 
+        self.lock = threading.Lock() # to control access to serial port
+
         # pumps must start in input state 
         # plunger position in steps, 0 means when the plunger is up, no liquid inside
-        self.pumps = {'P0' : {'id':0, 'valve': 'input', 'plunger' : 0} ,
-                        'P1': {'id':1, 'valve': 'input', 'plunger' : 0},
-                        'P2': {'id':2, 'valve': 'input', 'plunger' : 0},
-                        'P3': {'id':3, 'valve': 'input', 'plunger' : 0},
-                        'P4': {'id':4, 'valve': 'input', 'plunger' : 0},
-                        'P7': {'id':7, 'valve': 'input', 'plunger' : 0}}
+        self.pumps = {'P0' : {'id':0, 'syringe':5, 'valve': 'input', 'plunger' : 0} ,
+                        'P1': {'id':1, 'syringe':5, 'valve': 'input', 'plunger' : 0},
+                        'P2': {'id':2, 'syringe':5, 'valve': 'input', 'plunger' : 0},
+                        'P3': {'id':3, 'syringe':5, 'valve': 'input', 'plunger' : 0},
+                        'P4': {'id':4, 'syringe':5, 'valve': 'input', 'plunger' : 0},
+                        'P7': {'id':7, 'syringe':5, 'valve': 'input', 'plunger' : 0}}
 
 
     def wait_response(self):
         ''' Syncs pumps and Arduino '''
 
-        response = self.pumps_ser.readline().strip()
+        self.lock.acquire()
+        try:
+            response = self.pumps_ser.readline()#.strip()
+            print(response)
+        finally:
+            self.lock.release()
 
 
     def rotate_valve(self, pump, valve):
         ''' positions the valve to input or output '''
 
+        pump_id = self.pumps[pump]['id'] 
+        code = int( str(pump_id) + str(1) )
+
         if valve is 'input' and self.pumps[pump]['valve'] is not 'input':
-            command = "P%d M1 C0 D0 S400 E2208\n" % ( self.pumps[pump]['id'] )
+            command = "P%d M1 C%d D0 S400 E2208\n" % ( pump_id, code )
             self.pumps_ser.write(command.encode())
             self.wait_response()
             self.pumps[pump]['valve'] = 'input'
 
 
         if valve is 'output' and self.pumps[pump]['valve'] is not 'output':
-            command = "P%d M1 C0 D1 S400 E2208\n" % ( self.pumps[pump]['id'] )
+            command = "P%d M1 C%d D1 S400 E2208\n" % ( pump_id, code )
             self.pumps_ser.write(command.encode())
             self.wait_response()
             self.pumps[pump]['valve'] = 'output'
@@ -46,6 +56,9 @@ class PumpsCtl:
     def actuate_pump(self, pump, speed, abs_steps):
         ''' abs_steps means absolute steps position, being 1 fully pushed and 100000 fully pulled'''
 
+        pump_id = self.pumps[pump]['id'] 
+        code = int( str(pump_id) + str(0) )
+        
         if abs_steps < 1: abs_steps = 1
         if abs_steps > 100000: abs_steps = 100000
         if speed < 20: speed = 20
@@ -55,7 +68,7 @@ class PumpsCtl:
 
         required_steps = abs( abs_steps - self.pumps[pump]['plunger'] )
 
-        command = "P%d M0 C0 D%d S%d E%d\n" % ( self.pumps[pump]['id'], direction, speed, required_steps )
+        command = "P%d M0 C%d D%d S%d E%d\n" % ( pump_id, code, direction, speed, required_steps )
         self.pumps_ser.write(command.encode())
         self.wait_response()
         self.pumps[pump]['plunger'] = abs_steps
