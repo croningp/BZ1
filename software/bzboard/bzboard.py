d0ff5f3674d5dc513eec28dc3c5993ab70d5771c
Andrew Quinn <2186149q@student.gla.ac.uk>
2018-10-24 11:12:37 +0100

    Update bzboard.py to create a random pattern

--- software/bzboard/bzboard.py
+++ software/bzboard/bzboard.py
@@ -9,12 +9,14 @@ If you want better functionality, error handling,... check the other one
 '''
 
 import serial, time, json, random
-
+#need csv
+import csv
+from datetime import datetime
 
 class BZBoard:
 
 
-    def __init__(self, port):
+    def __init__(self, port): # creates objects to pass through function for each self
 
         self.ser = serial.Serial(port, 9600, timeout=120)
         time.sleep(2) # serial docs recommend a wait just after connection
@@ -24,10 +26,10 @@ class BZBoard:
             "A1":[1, 8, 500],"A2":[1, 9, 565],"A3":[1,10, 525],"A4":[1,11, 545],"A5":[1,12, 590],
             "B1":[1,14, 510],"B2":[1,15, 600],"B3":[1,13, 580],"B4":[1, 0, 610],"B5":[1, 1, 565],
             "C1":[1, 2, 640],"C2":[1, 3, 605],"C3":[1, 4, 550],"C4":[1, 5, 530],"C5":[1, 6, 620],
-            "D1":[1, 7, 575],"D2":[0,4, 595],"D3":[0, 8, 650],"D4":[0, 9, 545],"D5":[0,10, 510],
+            "D1":[1, 7, 575],"D2":[0, 4, 595],"D3":[0, 8, 650],"D4":[0, 9, 545],"D5":[0,10, 510],
             "E1":[0,11, 565],"E2":[0,12, 550],"E3":[0,13, 565],"E4":[0,14, 560],"E5":[0,15, 520]
             }
-
+        # inactive but not off stirers for mixing
 
         # this matrix stores if the motors are enabled (1) or disabled (0)
         # before writting to serial we check this to not sent writes not needed
@@ -46,28 +48,28 @@ class BZBoard:
         self.disable_all()
         self.ser.close()
         del self.ser
-
+        # disbales all motors and clears variables
 
     def close(self):
 
         self.disable_all()
         self.ser.close()
         del self.ser
-
+        # seems to do the same 
 
     def pattern_from_file(self, patternfile):
 
         with open(patternfile) as f:
             return json.load(f)
-
+        # opens pattern 
 
     def activate_motor(self, motor_code, speed=None):
         ''' code as in A1 or C2 as marked in the actual board. See the dict motors'''
 
         if speed == None:
-            shield, pin, speed = self.motors[motor_code] 
+            shield, pin, speed = self.motors[motor_code] # sets motor speed if defined in pattern as 0 
         else:
-            shield, pin, _  = self.motors[motor_code] 
+            shield, pin, _  = self.motors[motor_code] # now if not 0 so loop isn't running over null
         
         # because sometimes we can send here a speed 0 to disable it
         # the following if will be longer than expected
@@ -78,27 +80,27 @@ class BZBoard:
             # and then we send the desired speed
             command += "A%d P%d S%d\n" % ( shield, pin, speed )
             self.matrix[motor_code] = 1 # mark it as enabled
-            self.ser.write(command.encode())
+            self.ser.write(command.encode()) # encode helps program run 
 
-        elif self.matrix[motor_code] < 1 and speed == 0: # we keep it disabled
+        elif self.matrix[motor_code] < 1 and speed == 0: # we keep it disabled (it's off with 0 speed)
             pass
 
         elif self.matrix[motor_code] > 0 and speed > 0: # just update speed
             command = "A%d P%d S%d\n" % ( shield, pin, speed )
             self.ser.write(command.encode())
 
-        elif self.matrix[motor_code] > 0 and speed == 0: # we disable it
+        elif self.matrix[motor_code] > 0 and speed == 0: # we disable it (on with no speed)
             self.disable_motor(motor_code)
 
         else:
-            print("something bad happened when updating a motor")
+            print("something bad happened when updating a motor") # broken
 
 
     def activate_all(self, speed=None):
 
-        for key in self.motors.keys():
+        for key in self.motors.keys(): # set to min stiring speed 
             self.activate_motor(key, speed)
-            self.ser.flush()
+            self.ser.flush() #flush variables
 
 
     def activate_pattern(self, pattern, speed=None):
@@ -110,7 +112,31 @@ class BZBoard:
             if pattern[i] == 1:
                 self.activate_motor(i, speed*2) # x5 before
             else:
-                self.activate_motor(i, speed*0) # x1 before
+                self.activate_motor(i, speed*0) # x1 before if motor is off speed off
+    
+
+    def activate_rand(self, speed):
+        # need variable titles
+        exp_time = datetime.now().strftime('%Y-%m-%d %H:%M')
+        # opens/creates csv in append mode
+        rand_list = open(exp_time +'.csv','a')
+        # random 5*5 speed
+
+        self.rand_speed = { 
+                    "A1":randint(0,4000),"A2":randint(0,4000),"A3":randint(0,4000),"A4":randint(0,4000),"A5":randint(0,4000),
+                    "B1":randint(0,4000),"B2":randint(0,4000),"B3":randint(0,4000),"B4":randint(0,4000),"B5":randint(0,4000),
+                    "C1":randint(0,4000),"C2":randint(0,4000),"C3":randint(0,4000),"C4":randint(0,4000),"C5":randint(0,4000),
+                    "D1":randint(0,4000),"D2":randint(0,4000),"D3":randint(0,4000),"D4":randint(0,4000),"D5":randint(0,4000),
+                    "E1":randint(0,4000),"E2":randint(0,4000),"E3":randint(0,4000),"E4":randint(0,4000),"E5":randint(0,4000)
+                    }
+
+        rand_list.write(self.rand_speed)
+        #appends list 
+    	# write random values to stirer matrix
+        for i in self.motors.keys():
+            speed = self.rand_speed[i]
+            self.activate_motor(i, speed)
+            
 
 
     def disable_motor(self, motor_code):
