5e032850acc606671c09dd42da2ecb8e5906ae30
Andrew Quinn <2186149q@student.gla.ac.uk>
2018-11-06 11:32:28 +0000

    Replace automatedBZ.py

--- software/automatedBZ.py
+++ software/automatedBZ.py
@@ -3,6 +3,10 @@ from pumpsctl.pumpsctl import PumpsCtl
 from bzboard.bzboard import BZBoard
 from img_proc.record_cam import RecordVideo
 from datetime import datetime
+import os, sys
+sys.path.append(os.path.abspath('..'))
+from tools import volctl
+import pickle
 
 
 class AutomatedPlatform():
@@ -26,25 +30,33 @@ class AutomatedPlatform():
         self.kbro3 =       {'pump':'P5', 'quantity':19,  'speedIn':110,'speedOut':110}
         self.kbro3_clean = {'pump':'P5', 'quantity':15,  'speedIn':110,'speedOut':110}
         self.malonic =     {'pump':'P3', 'quantity':18,  'speedIn':60, 'speedOut':60}
-        self.water_clean = {'pump':'P4', 'quantity':50,  'speedIn':60, 'speedOut':60} # what does clean and waste do here
+        self.water_clean = {'pump':'P4', 'quantity':50,  'speedIn':60, 'speedOut':60} 
 
 
     def perform_experiment(self, water=15, ferroin=2.5, h2so4=12.5, kbro3=19, malonic=18):
 
+        #set experiental parameters 
         self.water['quantity'] = water
         self.ferroin['quantity'] = ferroin
         self.h2so4['quantity'] = h2so4
         self.kbro3['quantity'] = kbro3
         self.malonic['quantity'] = malonic
-        
+
+        #calc volume for one experiment including cleaning
         vol_check_water =   self.water['quantity'] + 3*self.water_clean['quantity']
         vol_check_ferroin = self.ferroin['quantity'] 
-        vol_check_h2so4 =   self.h2so4['quantity'] + 2*self.h2so4_clean['quantity']
+        vol_check_h2so4 =   self.h2so4['quantity'] + 2*self.h2so4_clean['quantity']          
         vol_check_kbro3 =   self.kbro3['quantity'] + 2*self.kbro3_clean['quantity']
-        vol_check_malonic = self.malonic['quantity'] 
+        vol_check_malonic = self.malonic['quantity']             
         vol_check_waste =   vol_check_water + vol_check_ferroin + vol_check_h2so4 + vol_check_kbro3 + vol_check_malonic
-        self.p.pre_exp_check(vol_check_water, vol_check_ferroin, vol_check_h2so4, vol_check_kbro3, vol_check_malonic, vol_check_waste)
-        
+        #update required experiment volumes  (expvol) in volctl
+        self.p.expvolinput(self,vol_check_water,vol_check_ferroin,vol_check_h2so4,vol_check_kbro3,vol_check_malonic,vol_check_waste)
+        #call countdown and print how many experiments are left
+        self.p.countdown()
+        #check enough for whole experiment to run and direct to reset if not
+        self.p.pre_exp_check()
+
+
         #dispense the BZ recipe into the arena
         self.p.pump_multiple(self.water, self.malonic, self.kbro3, self.h2so4, 
                 self.ferroin)
@@ -84,5 +96,5 @@ if __name__ == "__main__":
 
     ap = AutomatedPlatform()
 
-    for i in range(2): # number of experiments
+    for i in range(1): # number of experiments
         ap.perform_experiment()
