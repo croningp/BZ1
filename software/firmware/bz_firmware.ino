dead2b15909b9da493d2e75e3f297fcd0528fc60
Juan Manuel Parrilla <juanma@chem.gla.ac.uk>
2019-01-31 16:08:29 +0000

    BZ motors firmware has been updated to use adafruit motor shield instead of our home made PCB

--- software/firmware/bz_firmware.ino
+++ software/firmware/bz_firmware.ino
@@ -1,56 +1,62 @@
 /*
-This code tries to be a minimal firmware implementation.
-It uses: https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library/
-In particular the file pwmtest.ino adding basic serial reading.
+https://learn.adafruit.com/adafruit-motor-shield-v2-for-arduino
+Code based on the above adafruit lib
 It doesn't have callbacks or error checking, so the user must be
 sure everything sent here is OK beforehand.
 
-The commands are like A_ P_ S_
-Each argument starts with a capital letter, followed by numbers (value of arg), no space
+The commands are like A_ M_ D_ S_
+Each argument starts with a capital letter, followed by numbers (value of arg)
 Each argument is separated by 1 space
-A means Adafruit board, it must be 0 or 1
-P means pins, it must be a number between 0 and 15
-S means speed, which will define the PWM signal. Max in 4096
+A means Adafruit board. Depends on the assembly, at the moment 7 (0 to 6) (max is 32)
+M means motor. Each adafruit board can hold 4 motors. [1-4]
+D meand direction. Either 0 or 1.
+S means speed, which will define the PWM signal. Max in 255
 Commands must end with '\n'
+Example: "A4 M2 D0 S100\n"
 */
 
-#include <Adafruit_PWMServoDriver.h>
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
 
 #define BUFFER_SIZE 200
 
-// Init both adafruit boards
-Adafruit_PWMServoDriver pwm0 = Adafruit_PWMServoDriver(0x40);
-Adafruit_PWMServoDriver pwm1 = Adafruit_PWMServoDriver(0x41);
+// Init adafruit boards
+const int n_shields = 7;
+Adafruit_MotorShield AFMS[n_shields];
+
+// Pointer to motor that will be reused for each motor
+Adafruit_DCMotor *myMotor;
 
 
+// This function only reads from serial and stores the data read in buffer
 int read_command(char *buffer)
 {
 
-	static int pos = 0;
-	int rpos;
+  static int pos = 0;
+  int rpos;
 
-	if (Serial.available() > 0) {
-		char readch = Serial.read();
+  if (Serial.available() > 0) {
+    char readch = Serial.read();
 
-		switch (readch) {
+    switch (readch) {
 
-			case '\n':
-				rpos = pos;
-				pos = 0; // Reset position index ready for next time
-				return rpos;
+      case '\n':
+        rpos = pos;
+        pos = 0; // Reset position index ready for next time
+        return rpos;
 
-			default:
+      default:
 
-				if (pos < BUFFER_SIZE -1) {
-					buffer[pos++] = readch;
-					buffer[pos] = '\0';
-				} else {
-					Serial.println("666"); //buffer overflow
-				}
-		}
-	}
-	// no end line or char found, return -1
-	return -1;
+        if (pos < BUFFER_SIZE -1) {
+          buffer[pos++] = readch;
+          buffer[pos] = '\0';
+        } else {
+          Serial.println("666"); //buffer overflow
+        }
+    }
+  }
+  // no end line or char found, return -1
+  return -1;
 
 }
 
@@ -58,38 +64,46 @@ int read_command(char *buffer)
 void parse_command(char* command) 
 {
 
-	char* parameter;
-	parameter = strtok(command, " ");
-	long shield, pin, speed;
+  char* parameter;
+  parameter = strtok(command, " ");
+  long shield, motor, direction, speed;
 
-	while (parameter != NULL) {
+  while (parameter != NULL) {
 
-		switch(parameter[0]) {
+    switch(parameter[0]) {
 
-			case 'A':
-				shield = strtol(parameter+1, NULL, 10);
-				break;
+      case 'A':
+        shield = strtol(parameter+1, NULL, 10);
+        break;
 
-			case 'P':
-				pin = strtol(parameter+1, NULL, 10);
-				break;
+      case 'M':
+        motor = strtol(parameter+1, NULL, 10);
+        myMotor = AFMS[shield].getMotor(motor);
+        break;
 
-			case 'S':
-				speed = strtol(parameter+1, NULL, 10);
-				break;
+      case 'D':
+        direction = strtol(parameter+1, NULL, 10);
+        if (direction==0) {
+          myMotor->run(BACKWARD);
+        } else{
+          myMotor->run(FORWARD);
+        }
+        break;
 
-		}
-		parameter = strtok(NULL, " ");
-	}
-	
-	for (int x=0; x < BUFFER_SIZE; x++)
-		command[x] = '\0';
-	
-  if (shield == 0)
-    pwm0.setPWM(pin, 0, speed);
+      case 'S':
+        speed = strtol(parameter+1, NULL, 10);
+        myMotor->setSpeed(speed);
+        if (speed == 0) {
+          myMotor->run(RELEASE);
+        }
+        break;
 
-  if (shield == 1)
-    pwm1.setPWM(pin, 0, speed);
+    }
+    parameter = strtok(NULL, " ");
+  }
+  
+  for (int x=0; x < BUFFER_SIZE; x++)
+    command[x] = '\0';
 
 }
 
@@ -97,14 +111,15 @@ void parse_command(char* command)
 void setup() 
 {
   
-  Serial.begin(19200);
+  Serial.begin(9600);
   Serial.flush();
 
-  pwm0.begin();
-  pwm0.setPWMFreq(100);  //Max is 1600, Kevin was using 1000
-  pwm1.begin();
-  pwm1.setPWMFreq(100);
-
+  uint8_t base = 0x60;
+  for (int i=0; i<n_shields; i++){
+    AFMS[i] = Adafruit_MotorShield(base+i);
+    AFMS[i].begin();
+  }
+  
 }
 
 
